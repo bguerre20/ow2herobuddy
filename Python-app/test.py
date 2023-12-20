a638ee655bc5fa7ad0668605041f8a54abf048b8
bguerre20 <bguerre12@gmail.com>
2023-12-19 18:26:22 -0800

    Updated json object to include hero roles, updated algorithm to only suggest heroes matching the players role

--- Python-app/test.py
+++ Python-app/test.py
@@ -1,30 +1,30 @@
 import json
 from collections import Counter
 
-def find_common_counters(enemyHeroes, jsonObject):
-    # Convert the JSON object to a Python dictionary
-    heroes_data = jsonObject
-    
+def find_common_counters(enemyHeroes, jsonObject, playerRole):
     # Create a dictionary with hero names as keys and their counters as values
-    hero_counters = {hero['hero']: hero['counters'] for hero in heroes_data}
-    
+    hero_counters = {hero['hero']: hero['counters'] for hero in jsonObject}
+    # Create a dictionary with hero names as keys and their roles as values
+    hero_roles = {hero['hero']: hero['role'] for hero in jsonObject}
+
     # Initialize a list to store all counters for the enemy heroes
     all_counters = []
     
     # Loop through the enemy heroes and add their counters to the list
     for hero in enemyHeroes:
         if hero in hero_counters:
-            all_counters.extend(hero_counters[hero])
+            # Check the role of each counter before adding it to the list
+            all_counters.extend(counter for counter in hero_counters[hero] if counter in hero_roles and hero_roles[counter] == playerRole)
     
     # Use Counter to find the most common counters
     counter = Counter(all_counters)
     
     # Return the top 3 most common counters
-    return [hero for hero, count in counter.most_common(6)]
+    return [hero for hero, count in counter.most_common(3)]
 
 # Test the function
-enemyHeroes = ["Pharah", "Mercy", "Ana", "Winston","Widowmaker"]
+enemyHeroes = ["Hanzo", "Zenyatta", "Mercy", "Wrecking Ball","Echo"]
 with open('C:\\Users\\bryan\\Desktop\\CodeProjects\\ow2herobuddy\\Python-app\\HeroCounterDB.json', 'r') as f:
     jsonObject = json.load(f) 
 
-print(find_common_counters(enemyHeroes, jsonObject))
+print(find_common_counters(enemyHeroes, jsonObject,playerRole='damage'))
